b71f17cea048b2bbd72858ef905e0b661ef32caf
Roman Kuzero <rombeldan@gmail.com>
2024-02-12 21:06:57 +0200

    Solve Coding Excercise 3: Frequency Counter - sameFrequency

---
+++ section-6-optional-challenges/coding-excercise-3-frequency-counter.js
@@ -0,0 +1,32 @@
+function sameFrequency(int1, int2) {
+  const str1 = int1.toString();
+  const str2 = int2.toString();
+
+  // Edge case: If the lengths differ, the frequency of digits cannot match
+  if (str1.length !== str2.length) return false;
+
+  const frequencyCounter1 = {};
+  const frequencyCounter2 = {};
+
+  // Count the frequency of each digit in both numbers
+  for (let digit of str1) {
+    frequencyCounter1[digit] = (frequencyCounter1[digit] || 0) + 1;
+  }
+  for (let digit of str2) {
+    frequencyCounter2[digit] = (frequencyCounter2[digit] || 0) + 1;
+  }
+
+  // Compare the frequency of each digit in both numbers
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(sameFrequency(12, 21));
