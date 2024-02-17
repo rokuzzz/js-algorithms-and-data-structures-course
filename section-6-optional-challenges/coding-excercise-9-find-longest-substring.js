60bc73773862c958a4b3f9efcbfaac9dc5f6a06f
Roman Kuzero <rombeldan@gmail.com>
2024-02-17 19:36:40 +0200

    Solve Coding Excercise 9: Sliding Window - findLongestSubstring

---
+++ section-6-optional-challenges/coding-excercise-9-find-longest-substring.js
@@ -0,0 +1,26 @@
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+// Test cases
+console.log('1', findLongestSubstring('')); // 0
+console.log('2', findLongestSubstring('rithmschool')); // 7
+console.log('3', findLongestSubstring('thisisawesome')); // 6
+console.log('4', findLongestSubstring('thecatinthehat')); // 7
+console.log('5', findLongestSubstring('bbbbbb')); // 1
+console.log('6', findLongestSubstring('longestsubstring')); // 8
+console.log('7', findLongestSubstring('thisishowwedoit')); // 6
