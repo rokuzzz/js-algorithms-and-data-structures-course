b08d1faf369930e4f36bfdabd08b684085fc6a3b
Roman Kuzero <rombeldan@gmail.com>
2024-03-05 20:02:26 +0200

    Develop mergeSortedArrays for use in upcoming mergeSort implementation

---
+++ section-15-merge-sort/merge-sorted-arrays.js
@@ -0,0 +1,28 @@
+function mergeSortedArrays(firstArray, secondArray) {
+  let firstIndex = 0;
+  let secondIndex = 0;
+  const mergedResult = [];
+
+  while (firstIndex < firstArray.length && secondIndex < secondArray.length) {
+    if (
+      firstArray[firstIndex] < secondArray[secondIndex] ||
+      secondIndex >= secondArray.length
+    ) {
+      mergedResult.push(firstArray[firstIndex++]);
+    } else {
+      mergedResult.push(secondArray[secondIndex++]);
+    }
+  }
+
+  return mergedResult.concat(
+    firstArray.slice(firstIndex),
+    secondArray.slice(secondIndex)
+  );
+}
+
+// Test cases
+console.log('1: ', mergeSortedArrays([3, 4, 6], [1, 2, 5, 7])); // [1, 2, 3, 4, 5, 6, 7]
+console.log('2: ', mergeSortedArrays([-2, 0, 2], [-3, 1, 3])); // [-3, -2, 0, 1, 2, 3]
+console.log('3: ', mergeSortedArrays([10, 20], [15, 25, 30])); // [10, 15, 20, 25, 30]
+console.log('4: ', mergeSortedArrays([], [1, 2, 3])); // [1, 2, 3]
+console.log('5: ', mergeSortedArrays([5, 5, 5, 5], [5, 5])); // [5, 5, 5, 5, 5, 5]
